5d761e0de03d3bafa00e43cd7e8ae2e23de57aa3
jalvarezotero <you@example.com>
2019-04-10 10:47:03 +0200

    creado

---
+++ extrabonoloto/ExtraBonoloto.java
@@ -0,0 +1,10 @@
+package extrabonoloto;
+
+public class ExtraBonoloto {
+
+    public static void main(String[] args) {
+        Loto panel = new Loto();
+        panel.setVisible(true);
+    }
+
+}
